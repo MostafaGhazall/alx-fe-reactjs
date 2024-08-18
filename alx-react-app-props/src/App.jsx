63eb5cd4ed3dbf74704c5ff2f3f83ae21e2f92a2
Mostafa <mostafaghazal210@gmail.com>
2024-08-18 04:06:40 +0300

    updates

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,7 +8,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'
 import ProfilePage from './components/ProfilePage';
-import UserContext from './components/UserContext';
+import UserContext from './UserContext';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,11 +16,9 @@ function App() {
 
   return (
     <>
-      <div>
         <UserContext.Provider value={userData}>
           <ProfilePage />
         </UserContext.Provider>
-      </div>
       <div>
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
