92020c7b449279a9739ebc307828fd97af6e9c6f
Mostafa <mostafaghazal210@gmail.com>
2024-08-18 03:22:03 +0300

    updates

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,13 +6,17 @@ import './App.css'
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
-import UserProfile from './UserProfile'
+import UserProfile from './UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Counter />
+      </div>
       <div>
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
